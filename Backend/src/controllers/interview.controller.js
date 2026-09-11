ba9517152cdad43e9ec76e694c49513bd380414f
Chetan Dhapola <chetandhapola44@gmail.com>
2026-09-12 01:11:59 +0530

    Fix PDF parser for Vercel

--- Backend/src/controllers/interview.controller.js
+++ Backend/src/controllers/interview.controller.js
@@ -37,11 +37,7 @@ async function generateInterViewReportController(req, res) {
                 })
             }
 
-            const resumeContent = await (
-                new pdfParse.PDFParse(
-                    Uint8Array.from(req.file.buffer)
-                )
-            ).getText()
+            const resumeContent = await pdfParse(req.file.buffer)
 
             resumeText = resumeContent.text
         }
